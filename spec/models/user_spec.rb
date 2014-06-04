eb3c19278aeb95fd387dd4b75841fe2a59115cad
dobestan <dobestan@gmail.com>
2014-06-04 23:07:34 +0900

    add very basic user model : including email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,63 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new( email: "example_user@example_email.com",
+                       password: "foobar",
+                       password_digest: "foobar" )
+  end
+
+  subject { @user }
+
+  it { should respond_to :email }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+  it { should respond_to :password_digest }
+
+  describe "when Email" do
+    describe "is not presence" do
+      before { @user.email = "" }
+      it { should_not be_valid }
+    end
+
+    describe "format is invalid" do
+      invalid_addresses = %w[ user@foo,com
+                              user_at_foo.org
+                              example.user@foo.
+                              foo@bar_baz.com
+                              foo@bar+baz.com
+                            ]
+
+      invalid_addresses.each do |invalid_address|
+        before { @user.email = invalid_address }
+        it { should_not be_valid }
+      end
+    end
+
+    describe "format is valid" do
+      valid_addresses = %w[ user@foo.COM
+                            A_US-ER@f.b.org
+                            first.last@foo.jp
+                            a+b@baz.cn
+                          ]
+
+      valid_addresses.each do |valid_address|
+        before { @user.email = valid_address }
+        it { should be_valid }
+      end
+    end
+
+    describe "is already taken" do
+      before do
+        user_with_same_email = @user.dup
+        user_with_same_email.email = @user.email.upcase
+        user_with_same_email.save
+      end
+
+      it { should_not be_valid }
+    end
+  end
+
+  describe "Password" do
+  end
 end
